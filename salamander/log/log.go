0ea891abcd27b35d842ecfe46dd48780ebc27691
nasa9084 <nasa9084@live.jp>
2017-10-16 17:13:36 +0900

    add: logger utility

---
+++ salamander/log/log.go
@@ -0,0 +1,13 @@
+package log
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	// Info logger
+	Info  = log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	// Error logger
+	Error = log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lshortfile)
+)
