02eca3a551bae4a6f34b24633c2e697e7414ac45
nasa9084 <nasa9084@live.jp>
2017-11-13 17:36:19 +0900

    update: no wrap error

--- salamander/model/model.go
+++ salamander/model/model.go
@@ -2,14 +2,12 @@ package model
 
 import (
 	"database/sql"
-
-	"github.com/pkg/errors"
 )
 
 func checkResult(r sql.Result) error {
 	rowsAffected, err := r.RowsAffected()
 	if err != nil {
-		return errors.Wrap(err, `checking rows affected`)
+		return err
 	}
 	if rowsAffected == 0 {
 		return ErrNoRowsAffected
