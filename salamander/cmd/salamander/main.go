69c817c9b0757188e1937d7a8c351329003fdfd9
nasa9084 <nasa9084@live.jp>
2017-12-07 13:19:14 +0900

    some structure fix

--- salamander/cmd/salamander/main.go
+++ salamander/cmd/salamander/main.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"database/sql"
-	"log"
 	"os"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/jessevdk/go-flags"
 	"github.com/nasa9084/salamander/salamander"
+	"github.com/nasa9084/salamander/salamander/log"
+	"github.com/nasa9084/salamander/salamander/middleware"
+	"github.com/pkg/errors"
 )
 
 type options struct {
-	Listen        string `short:"l" long:"listen" env:"SALAMANDER_LISTEN" default:":8080" description:"listening address"`
-	MySQLAddr     string `long:"mysql-addr" env:"MYSQL_ADDR" default:"localhost:3306" description:"hostname or IP address and port of MySQL"`
-	MySQLDB       string `long:"mysql-db" env:"MYSQL_DB" default:"salamander" description:"database name for this application"`
-	MySQLUser     string `long:"mysql-user" env:"MYSQL_USER" default:"root" description:"MySQL username"`
-	MySQLPassword string `long:"mysql-password" env:"MYSQL_PASSWORD" default:"password" description:"MySQL password"`
+	Listen string `short:"l" long:"listen" env:"SALAMANDER_LISTEN" default:":8080" description:"listening address"`
+	MySQL  struct {
+		Addr     string `long:"mysql-addr" env:"MYSQL_ADDR" default:"localhost:3306" description:"hostname or IP address and port of MySQL"`
+		DB       string `long:"mysql-db" env:"MYSQL_DB" default:"salamander" description:"database name for this application"`
+		User     string `long:"mysql-user" env:"MYSQL_USER" default:"root" description:"MySQL username"`
+		Password string `long:"mysql-password" env:"MYSQL_PASSWORD" default:"password" description:"MySQL password"`
+	}
 }
 
 func main() { os.Exit(exec()) }
@@ -23,30 +27,49 @@ func main() { os.Exit(exec()) }
 func exec() int {
 	var opts options
 	if _, err := flags.Parse(&opts); err != nil {
-		log.Printf("%s", err)
+		log.Error.Printf("%s", err)
 		return 1
 	}
-	mysqlCfg := mysql.Config{
-		Net:       "tcp",
-		Addr:      opts.MySQLAddr,
-		User:      opts.MySQLUser,
-		Passwd:    opts.MySQLPassword,
-		DBName:    opts.MySQLDB,
-		ParseTime: true,
-	}
-	log.Printf("connect to MySQL DB: %s", mysqlCfg.FormatDSN())
-	db, err := sql.Open(`mysql`, mysqlCfg.FormatDSN())
+	db, err := openMySQL(opts)
 	if err != nil {
-		panic(err)
+		log.Error.Print(err)
+		return 1
 	}
 	s := salamander.NewServer(
 		db,
 		salamander.ListenAddr(opts.Listen),
+		salamander.Middlewares(
+			middleware.Logger,
+		),
 	)
-	log.Printf("server listening: %s", opts.Listen)
+	log.Info.Printf("server listening: %s", opts.Listen)
 	if err := s.Run(); err != nil {
-		log.Printf("%s", err)
+		log.Error.Printf("%s", err)
 		return 1
 	}
 	return 0
+}
+
+func openMySQL(opts options) (*sql.DB, error) {
+	mysqlCfg := getMySQLConfig(opts)
+	log.Info.Printf("connect to MySQL DB: %s", mysqlCfg.FormatDSN())
+	db, err := sql.Open(`mysql`, mysqlCfg.FormatDSN())
+	if err != nil {
+		return nil, errors.Wrap(err, `open`)
+	}
+	if err := db.Ping(); err != nil {
+		return nil, errors.Wrap(err, `ping`)
+	}
+	return db, nil
+}
+
+func getMySQLConfig(opts options) mysql.Config {
+	return mysql.Config{
+		Net:       "tcp",
+		Addr:      opts.MySQL.Addr,
+		User:      opts.MySQL.User,
+		Passwd:    opts.MySQL.Password,
+		DBName:    opts.MySQL.DB,
+		ParseTime: true,
+	}
 }
