ade46557f6f7f4bb5099a8d802b7682fb25d4bd6
nasa9084 <nasa9084@live.jp>
2017-12-12 11:45:51 +0900

    gofmt

--- salamander/log/log.go
+++ salamander/log/log.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	// Info logger
-	Info  = log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	Info = log.New(os.Stdout, "", log.Ldate|log.Ltime)
 	// Error logger
 	Error = log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lshortfile)
 )
